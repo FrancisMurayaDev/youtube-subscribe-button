b76a008810f24f902e80ec018d81c670aa672f5c
FrancisMurayaDev <francismurayadev@gmail.com>
2025-02-19 00:38:36 +0300

    fix bug

--- src/main.js
+++ src/main.js
@@ -1,11 +1,10 @@
 function clickButton() {
-  const buttonElement = document.querySelector('.js-subscribe-button');
-
-    if (buttonElement.innerHTML === 'Subscribe') {
-      buttonElement.innerHTML = 'Subscribed';
-      buttonElement.classList.add('is-subscribed');
-    } else {
-      buttonElement.innerHTML = 'Subscribe';
-      buttonElement.classList.remove('is-subscribed');
-    }
+  const buttonElement = document.querySelector(".js-subscribe-button");
+  if (buttonElement.innerHTML === "Subscribe") {
+    buttonElement.innerHTML = "Subscribed";
+    buttonElement.classList.add("is-subscribed");
+  } else {
+    buttonElement.innerHTML = "Subscribe";
+    buttonElement.classList.remove("is-subscribed");
+  }
 }
