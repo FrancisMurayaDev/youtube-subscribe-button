3b9952ce4d693a1b9aa54b72b99be1c2671bd96f
FrancisMurayaDev <francismurayadev@gmail.com>
2025-02-26 14:51:20 +0300

    add event listener

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
-function clickButton() {
-  const buttonElement = document.querySelector(".js-subscribe-button");
-  if (buttonElement.innerHTML === "Subscribe") {
-    buttonElement.innerHTML = "Subscribed";
-    buttonElement.classList.add("is-subscribed");
-  } else {
-    buttonElement.innerHTML = "Subscribe";
-    buttonElement.classList.remove("is-subscribed");
+
+const button = document.querySelector('.js-subscribe-button');
+button.onclick =  function clickButton() {
+    const buttonElement = document.querySelector(".js-subscribe-button");
+    if (buttonElement.innerHTML === "Subscribe") {
+      buttonElement.innerHTML = "Subscribed";
+      buttonElement.classList.add("is-subscribed");
+    } else {
+      buttonElement.innerHTML = "Subscribe";
+      buttonElement.classList.remove("is-subscribed");
+    }
   }
-}
+  
+
